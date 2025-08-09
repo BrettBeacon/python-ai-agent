94f85616792f8e8d0865231792649509cdc8013a
BrettBeacon <brett.beacon@gmail.com>
2025-08-09 17:09:51 +1000

    adding in files for the AI to manpilate, adding in functions to restrict the AI to certain directories.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import system_prompt
 
 
 def main():
@@ -37,6 +38,7 @@ def generate_content(client, messages, verbose=False):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     print("Response:")
     print(response.text)
