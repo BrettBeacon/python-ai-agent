67e5ebf40da5b1f1fa7ca2b80d7c404473400fc2
BrettBeacon <brett.beacon@gmail.com>
2025-08-12 15:43:14 +1000

    Adding functionality to ai agent in the form of running functions

--- main.py
+++ main.py
@@ -3,49 +3,71 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from config import system_prompt
 
+from prompts import system_prompt
+from call_function import call_function, available_functions
 
 def main():
     load_dotenv()
     
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = []
+
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here"')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
     
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    verbose = "--verbose" in args
-    clean_args = [arg for arg in args if not arg.startswith("--")]
-
-    user_prompt = " ".join(clean_args)
+    user_prompt = " ".join(args)
 
     if verbose:
-        print(f"User prompt: {user_prompt}")
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
             types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, verbose=verbose)
+    generate_content(client, messages, verbose)
 
-def generate_content(client, messages, verbose=False):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        ),
     )
-    print("Response:")
-    print(response.text)
     
     if verbose:
         print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
 
 if __name__ == "__main__":
     main()
